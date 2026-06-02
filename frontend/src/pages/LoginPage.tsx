ad9bbb2087504df32f6976a23b3ba5ab4e6c0491
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-06-02 22:29:02 +0100

    install tailwind and react router, setup simple login page
    currently functionless, need to sort out typings and all that and add functionality when I continue.

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const LoginPage = () => {
+  const [username, SetUsername] = useState('')
+  const [password, SetPassword] = useState('')
+
+  function updateUsername(e) {
+    SetUsername(e.target.value)
+  }
+
+  function updatePassword(e) {
+    SetPassword(e.target.value)
+  }
+
+  const onLoginClick = (e) => {
+    console.log("login button")
+  }
+
+  const onRegisterClick = () => {
+    console.log("register button")
+  }
+
+
+
+  return (
+    <div className='bg-gray-700 w-full h-screen flex justify-center items-center'>
+      <div className='bg-gray-500 w-[25vw] h-[40vh] flex flex-col gap-10'>
+        <h1 className='flex justify-center'>Login</h1>
+        <div className='flex px-5 py-3'>
+          <h2 className='mr-1'>Username</h2>
+          <input type='text' className='bg-white w-[80%]'/>
+        </div>
+        <div className='flex px-5 py-3'>
+          <h2 className='mr-2'>Password</h2>
+          <input type='text' className='bg-white w-[80%]' />
+        </div>
+        <div className='h-full flex justify-center items-center gap-10 '> 
+          <button className='bg-green-400 p-2 h-15 w-20' onClick={() => {onLoginClick(password)}}>Login</button>
+          <button className='bg-blue-400 p-2 h-15 w-20' onClick={() => {onRegisterClick()}}>Register</button>
+        </div>
+      </div>
+
+      
+
+    </div>
+  )
+}
+
+export default LoginPage
