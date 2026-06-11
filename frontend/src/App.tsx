2b2eae08755a5e04d37c4a24f3a44f53f4abde70
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-06-11 17:52:10 +0100

    homepage layout + header + footer
    title, includes header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
+import CreateProjectPage from './pages/CreateProjectPage'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path ='/home' element={<HomePage />} />
+        <Route path ='/project/create' element={<CreateProjectPage />} />
       </Routes>
       
 
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-700 flex-1'>Footer
+    
+    <div>test</div>
+    </div>
+  )
+}
+
+export default Footer
