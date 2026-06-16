d3c4d1de8ced2c2c7caa4a5d6abf60edbcd34c55
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-06-16 21:23:59 +0100

    project api part 2
    adds GET functionality. Planning to do the rest as a part 3 and then we can get back to hooking it up to display frontend.

--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -1,6 +1,14 @@
 import express from "express"
+import { createProject, deleteProject, getAllProjects, getProject, updateProject } from "../controllers/projectControllers.js";
 
 const projectRoutes = express.Router();
+
+projectRoutes.get("/", getAllProjects)
+projectRoutes.get("/:id", getProject)
+projectRoutes.post("/", createProject)
+projectRoutes.delete("/:id", deleteProject)
+projectRoutes.put(":/id", updateProject)
+
 
 export default projectRoutes
 
